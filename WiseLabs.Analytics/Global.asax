223398fad74230b129e4fa2351ee599a798d4f49
Aymeric <aymeric.github@yopmail.com>
2015-12-28 11:26:43 -0300

    Move files in their own project and set up nuget spec.

--- WiseLabs.Analytics/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WiseLabs.Analytics.MvcApplication" Language="C#" %>
--- WiseLabs.Analytics/Properties/AssemblyInfo.cs
+++ WiseLabs.Analytics/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a2b93495-0b31-4239-ad3c-b8f082e98076")]
+[assembly: Guid("e93cfc71-8110-4eaa-ab5f-2953faf5b4f7")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
